8ebe0537bd7a11b7af880329588f87e9b5fb1255
Cristina Mantiñán <56038975+CrisMB-82@users.noreply.github.com>
2025-01-26 22:55:59 +0000

    Frist Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const who = [
+  "My cat",
+  "My computer",
+  "His parrot",
+  "The apocalypse",
+  "Nickelback"
+];
+const action = ["destroyed", "ate", "vomited on", "exploded"];
+const what = ["my notes", "my homework", "the tickets", "the bus"];
+const when = [
+  "before the class",
+  "when I was planning to do it",
+  "when I finished",
+  "during my lunch",
+  "while I was having coffee"
+];
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function generateExcuse() {
+  return `${getRandomElement(who)} ${getRandomElement(
+    action
+  )} ${getRandomElement(what)} ${getRandomElement(when)}.`;
+}
+
+function updateExcuse() {
+  const excuse = generateExcuse();
+  document.getElementById("excuse").textContent = excuse;
+}
+
+updateExcuse();
+
+document.getElementById("changeExcuse").addEventListener("click", updateExcuse);
